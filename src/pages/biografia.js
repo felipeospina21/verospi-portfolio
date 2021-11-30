7186c181bcacfa4562ed8253b0808c6312593088
Felipe Ospina <felipeospina21@hotmail.com>
2021-11-30 15:25:40 -0500

    update Seo

--- src/pages/biografia.js
+++ src/pages/biografia.js
@@ -1,24 +1,20 @@
 import React from "react"
 import { Center, Heading } from "@chakra-ui/react"
-import BioContainer from '../components/BioContainer'
-
+import BioContainer from "../components/BioContainer"
+import Seo from "../components/Seo"
 
 const biografiaPage = () => {
-
   return (
     <>
+      <Seo title="Bigorafía" />
       <Center m="2rem">
         <Heading as="h1" size="xl">
           Biografía
         </Heading>
       </Center>
-      <BioContainer/>
-
-    
+      <BioContainer />
     </>
   )
 }
-
-
 
 export default biografiaPage
--- src/pages/contacto.js
+++ src/pages/contacto.js
@@ -1,19 +1,24 @@
 import React from "react"
 import { Box, Center, Heading } from "@chakra-ui/react"
 import ContactInfo from "../components/ContactInfo"
+import Seo from "../components/Seo"
 
 const contactoPage = () => {
   return (
-    <Box>
-      <Center my="3rem">
-        <Heading as="h1" size="xl">
-          Contacto
-        </Heading>
-      </Center>
-      <Box mt="5rem" mb="20rem">
-        <ContactInfo dir="column" fontSize="lg" margin="1rem" />
+    <>
+      <Seo title="Bigorafía" />
+
+      <Box>
+        <Center my="3rem">
+          <Heading as="h1" size="xl">
+            Contacto
+          </Heading>
+        </Center>
+        <Box mt="5rem" mb="20rem">
+          <ContactInfo dir="column" fontSize="lg" margin="1rem" />
+        </Box>
       </Box>
-    </Box>
+    </>
   )
 }
 
